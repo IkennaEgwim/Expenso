b960d97eb474e8798d17dbf81a88372818ae5197
Ikenna Egwim <egwimik@yahoo.com>
2024-06-23 14:17:05 +0000

    Create Mmdels for expensese, category and budget

--- expenso_app/models.py
+++ expenso_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.category} - {self.amount}"
+
+class Budget(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.category} - {self.amount} ({self.start_date} to {self.end_date})"
