1a216e3248d6fc7a0388d59cd584f0779883c008
Ikenna Egwim <egwimik@yahoo.com>
2024-06-23 15:12:25 +0000

    Create and implement views and templates for registration, login, logout and home page

--- expenso/settings.py
+++ expenso/settings.py
@@ -48,6 +48,10 @@ INSTALLED_APPS = [
     'expenso_app',
 ]
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- expenso_app/views.py
+++ expenso_app/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'tracker/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'tracker/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
