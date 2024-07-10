50ee69aecdfd993987786211a21166e8965a51ae
Ikenna Egwim <egwimik@yahoo.com>
2024-07-10 11:58:00 +0000

    debug issues with laoding static

--- expenso_app/urls.py
+++ expenso_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
+    # path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('', views.home, name='home'),
     path('add_expense/', views.add_expense, name='add_expense'),
